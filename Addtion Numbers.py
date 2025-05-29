9783c0f3990b36ae9e57ce3e6353387fb2a36141
rbalaji865 <rbalaji865@gmail.com>
2025-05-29 19:09:09 +0530

    Add files via upload

---
+++ Addtion Numbers.py
@@ -0,0 +1,16 @@
+a=input("Enter first number: ")
+b=input("Enter second number: ")
+
+a=int(a)
+b=int(b)
+
+
+add = a+b
+sub = a-b
+mul = a*b
+div = (a/b)
+
+print("addition", add)
+print("subtraction", sub)
+print("multiplication", mul)
+print("division", div)
---
+++ Name addition.py
@@ -0,0 +1,6 @@
+a=input("enter first name:  ")
+b=input("enter last name:  ")
+
+c=  a + b
+
+print("Hi", c, "to the world of Python")
